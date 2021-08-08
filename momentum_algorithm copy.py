f73fa2b5699f399615e3fa198ed05483b16350f7
JakeL1001 <jakelyell@iinet.net.au>
2021-08-08 22:47:23 +0800

    streamlined ability to test

--- momentum_algorithm copy.py
+++ momentum_algorithm copy.py
@@ -5,14 +5,16 @@ from talib.abstract import *
 from gemini_modules import engine
 
 # read in data preserving dates
-df = pd.read_csv("data/USDT_BTC.csv", parse_dates=[0])
 
 # globals
-training_period = int(input("short training period: "))
-long_training_period = int(input("long training period: "))
+print("Enter the number of the algorithm to use")
+algorithm_choice = int(input("1: Standard Rolling Average. 2: Moving Average Crossover. 3: Exponential Weighted Moving Average"))
+training_period = int(input("Training period: "))
+if algorithm_choice == 2:
+    long_training_period = int(input("Long training period: "))
 print("Loading...")
 #backtesting
-backtest = engine.backtest(df)
+
 
 '''Algorithm function, lookback is a data frame parsed to function continuously until end of initial dataframe is reached.'''
 def logic(account, lookback):
@@ -66,11 +68,53 @@ def logic2(account, lookback):
         print(e)
     pass  # Handles lookback errors in beginning of dataset
 
+def logic3(account, lookback):
+    try:
+        today = len(lookback)-1
+        if(today > training_period): 
+            exp_price_moving_average = lookback['close'].ewm(span=training_period).mean()[today]  # update PMA
+            volumn_moving_average = lookback['volume'].rolling(window=training_period).mean()[today]  # update VMA
 
-if __name__ == "__main__":
-    backtest.start(100, logic2)
-    backtest.results()
-    print("short training period = " + str(training_period))
-    print("long training period = " + str(long_training_period))
-    print("Moving Average Crossover")
-    backtest.chart()
+            if(lookback['close'][today] < exp_price_moving_average):
+                if(lookback['volume'][today] > volumn_moving_average):
+                    if(account.buying_power > 0):
+                        account.enter_position('long', account.buying_power, lookback['close'][today])
+                        #print("bought at" + str(lookback["date"][today]))
+            else:
+                if(lookback['close'][today] > exp_price_moving_average):
+                    if(lookback['volume'][today] < volumn_moving_average):
+                        for position in account.positions:
+                                account.close_position(position, 1, lookback['close'][today])
+                                #print("sold at" + str(lookback["date"][today]))
+    except Exception as e:
+        print(e)
+    pass  # Handles lookback errors in beginning of dataset
+
+# df = pd.read_csv("data/USDT_XRP.csv", parse_dates=[0])
+# df2 = pd.read_csv("data/USDT_BTC.csv", parse_dates=[0])
+# df3 = pd.read_csv("data/USDT_DOGE.csv", parse_dates=[0])
+# df4 = pd.read_csv("data/USDT_ETH.csv", parse_dates=[0])
+# df5 = pd.read_csv("data/USDT_LTC.csv", parse_dates=[0])
+list_of_coins = ["USDT_XRP", "USDT_BTC", "USDT_DOGE", "USDT_ETH", "USDT_LTC"]
+
+for x in list_of_coins: #allow choice of number of coins to use
+    df = pd.read_csv("data/" + x + ".csv", parse_dates=[0])
+    backtest = engine.backtest(df)
+    if __name__ == "__main__":
+        if algorithm_choice == 1:
+            backtest.start(100, logic)
+        elif algorithm_choice == 2:
+            backtest.start(100, logic2)
+        elif algorithm_choice == 3:
+            backtest.start(100, logic3)
+        print("Coin: " + x)
+        backtest.results()
+        print("short training period = " + str(training_period))
+        try:
+            print("long training period = " + str(long_training_period))
+        except:
+            pass
+        print("Loading next")
+        backtest.chart() #add all the coins to the one page
+print("Done")
+
