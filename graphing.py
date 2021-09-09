481e81180a1154b8963b6ef3f9877bee5f204a2e
Kanealex <72069221+Kanealex@users.noreply.github.com>
2021-09-09 13:15:28 +0800

    report

--- graphing.py
+++ graphing.py
@@ -7,7 +7,7 @@ df = pd.read_csv("results_overnight_kane.csv")
 
 df2 = df.groupby("Strategy_Name")
 
-tograph = "standard_no_volume"
+tograph = "Crossover moving average short"
 
 for a, b in df.groupby(["Strategy_Name","Coin"]):
     if(a[0] =='standard_no_volume' and tograph == 'standard_no_volume'):
@@ -17,8 +17,8 @@ for a, b in df.groupby(["Strategy_Name","Coin"]):
         value.to_csv("results.csv")
         plt.plot(value["x-axis"].values.tolist(), value["y-axis"].values.tolist(), label = str(a[1]))
         plt.xlabel('Price Window')
-        plt.ylabel('Percentage improvement on Buy and Hold')
-        plt.title('% Profit for Standard_no_volume vs Price Window')
+        plt.ylabel('Percentage Improvement on Buy and Hold')
+        plt.title('% Profit for Standard_No_Volume vs Price Window')
         plt.legend()
     if(a[0] =='standard' and tograph == 'standard_price'):
         yaxis = []
@@ -50,7 +50,6 @@ for a, b in df.groupby(["Strategy_Name","Coin"]):
         plt.ylabel('Percentage improvement on Buy and Hold')
         plt.title('% profit for standard')
         plt.legend()
-
     if(a[0] =='exp_no_volume' and tograph == 'exp_no_volume'):
         value = pd.DataFrame()
         value["y-axis"] = b["Strategy"] - b["Buy and Hold"]
@@ -62,7 +61,6 @@ for a, b in df.groupby(["Strategy_Name","Coin"]):
         plt.ylabel('Percentage improvement on Buy and Hold')
         plt.title('% Profit for exp_no_volume vs Price Window')
         plt.legend()
-
     if(a[0] =='Crossover moving average' and tograph == 'Crossover moving average long'):
         yaxis = []
         xaxis = []
@@ -93,10 +91,4 @@ for a, b in df.groupby(["Strategy_Name","Coin"]):
         plt.ylabel('Percentage improvement on Buy and Hold')
         plt.title('% profit for standard')
         plt.legend()
-
-
 plt.show()
-        
-    
-    
-    # print("breaks")
